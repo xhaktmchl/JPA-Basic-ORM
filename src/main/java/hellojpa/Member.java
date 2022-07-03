4cdab699e88d9475829baa6901b71f46253fb426
xhaktmchl <xhaktmchl@naver.com>
2022-07-03 15:58:51 +0900

    feat: DDL생성기능, @Column추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,13 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity //@Entity는 JPA에서 관리하는 엔티티임을 의미
+@Table // 엔티티를 원하는 테이블과 매핑
 public class Member {
 
     @Id // JPA 에게 주요키 임을 알리는 어노테이션
     private Long id;
+
+    @Column(name="name", nullable = true, length = 50, unique = true) // DDL 생성기능:테이블 컬럼에 옵션지정 가능
     private String name;
 
     // getter, setter
