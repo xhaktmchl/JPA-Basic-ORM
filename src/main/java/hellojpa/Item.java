e405623a5b4579b4900245f05363dac5216d997e
xhaktmchl <xhaktmchl@naver.com>
2022-07-17 19:37:24 +0900

    feat: 클래스당 테이블 상속관계 매핑전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계 매핑 전략
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 상속 관계 매핑 전략
 @DiscriminatorColumn // DTYPE 컬럼 자동 생성: 이 클래스를 상속받는 엔티티들을 구별해주는
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ITEM_ID")
